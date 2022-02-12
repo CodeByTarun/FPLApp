41d67ec11f4b66f858a349271b17693baa63782b
codebytarun <tarundeep1@hotmail.com>
2022-02-11 23:38:45 -0500

    Added Dicts

---
+++ App/Global/EnumsAndDicts.ts
@@ -0,0 +1,48 @@
+
+export const StatNames: {[key: string]: string} = 
+    {
+       "assists" : "Assists",
+        "bonus" : "Bonus",
+        "clean_sheets": "Clean Sheets",
+        "goals_conceded": "Goals Conceded",
+        "goals_scored": "Goals Scored",
+        "minutes": "Minutes",
+        "own_goals": "Own Goals",
+        "red_cards": "Red Cards",
+        "saves": "Saves",
+        "yellow_cards": "Yellow Cards",
+    }
+
+export const OverviewStats: {[key: string]: string} = 
+    {
+        "form" : "Form",
+        "now_cost" : "Cost",
+        "points_per_game" : "Points Per Game",
+        "selected_by_percent" : "Selected by Percent",
+        "total_points" : "Total Points",
+        "transfers_in": "Transfers In",
+        "transfers_in_event": "Transfers in Event",
+        "transfers_out": "Transfers Out",
+        "transfers_out_event": "Transfers out Event",
+        "value_form": "Value Form",
+        "value_season": "Value Season",
+        "goals_scored": "Goals Scored",
+        "assists": "Assists",
+        "clean_sheets": "Clean Sheets",
+        "goals_conceded": "Goals Conceded",
+        "own_goals": "Own Goals",
+        "penalties_saved": "Penalties Saved",
+        "penalties_missed": "Penalties Missed",
+        "yellow_cards": "Yellow Cards",
+        "red_cards": "Red Cards",
+        "saves": "Saves",
+        "bonus": "Bonus",
+        "bps": "bps",
+        "influence": "Influence",
+        "creativity": "Creativity",
+        "threat": "Threat",
+        "ict_index": "ICT Index",
+    }
+
+    
+
