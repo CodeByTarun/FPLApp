38be87a240a98964b725fcc3de8851bd798930df
Tarun Bola <tarundeep1@hotmail.com>
2022-04-18 13:26:05 -0400

    More testing.

--- Tests/Integration/Modals/PlayerModal.test.tsx
+++ Tests/Integration/Modals/PlayerModal.test.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import PlayerModal from "../../../App/Modals/PlayerModal";
+import { draftOverview, overview } from "../../SampleData/Overviews";
+import { allFixtures } from "../../SampleData/Fixtures";
+import { GetPlayerGameweekDataSortedByPosition } from "../../../App/Helpers/FplAPIHelpers";
+import { budgetLeaguePicks, draftLeaguePicks, gameweek } from "../LineupView/Gameweek32Data";
+import { TeamInfo, TeamTypes } from "../../../App/Store/teamSlice";
 
-test('', () => {
+let draftInfo: TeamInfo = { gameweek: 32, info: { id: 61187, isDraftTeam: true, isFavourite: true, name: 'Tarun' }, teamType: TeamTypes.Draft }
+
+const players = GetPlayerGameweekDataSortedByPosition(gameweek, overview, draftInfo, draftOverview, draftLeaguePicks, budgetLeaguePicks);
+
+test('one fixture modal renders correclty', () => {
+
+    const { queryByTestId, queryByText } = render(<PlayerModal overview={overview} fixtures={allFixtures} player={players![0]} teamInfo={draftInfo}/>)
+
+    expect(queryByTestId('closeButton')).toBeTruthy();
+
+});
+
+test('two fixtures modal renders correctly', () => {
+
+
+
+});
+
+test('if two fixtures but team type is fixure only show the stats for that game', () => {
+
 
     
-
 });
